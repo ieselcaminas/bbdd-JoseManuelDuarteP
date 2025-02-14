4bdb5de4eed90f75d68de108c0dd17e6819c753b
josduaper <josduaper@alu.edu.gva.es>
2025-02-14 12:55:01 +0100

    añadir usuario

--- src/main/ConectarSql.java
+++ src/main/ConectarSql.java
@@ -1,4 +1,7 @@
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ConectarSql {
 
@@ -17,7 +20,35 @@ public class ConectarSql {
         return connection;
     }
 
-    public static void main(String[] args) {
-        java.sql.Connection j = getConnection();
+    public static void main(String[] args) throws SQLException {
+        java.sql.Connection connection = getConnection();
+        Statement stmt = connection.createStatement();
+        //stmt.executeUpdate("CREATE TABLE T1 (c1 varchar(50))");
+        ResultSet rs = stmt.executeQuery("SELECT * FROM usuarios");
+        while (rs.next()) {
+            System.out.println(rs.getInt(1));
+            System.out.println("\t" + rs.getString(2));
+            System.out.println("\t" + rs.getString(3));
+        }
+        stmt.close();
+
+        insertUser();
+    }
+
+    public static void insertUser() throws SQLException {
+        String sql = "INSERT INTO usuarios (nombre, apellidos) VALUES ('Pedro', 'Peterson')";
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate(sql);
+        stmt.close();
+    }
+
+    public static void insertUserPreparedStatement() throws SQLException {
+        PreparedStatement stmt = null;
+        String sql = "INSERT INTO usuarios (nombre, apellidos) VALUES (?, ?)";
+        stmt = connection.prepareStatement(sql);
+        stmt.setString(1, "Manolo");
+        stmt.setString(2, "García");
+        stmt.executeUpdate();
+        stmt.close();
     }
 }
---
+++ src/main/GestionUsuarios.java
@@ -0,0 +1,75 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class GestionUsuarios {
+    public static void gestionMenu() throws SQLException {
+        Scanner sc = new Scanner(System.in);
+        int opcion = 0;
+
+        while (opcion != 3){
+            System.out.println("1 - Logearse");
+            System.out.println("2 - Nuevo usuario");
+            System.out.println("3 - Salir");
+
+            opcion = sc.nextInt();
+            if (opcion == 1){
+                //Llamada a método para logearase
+                boolean logeado = loggearse();
+                if (logeado) break;
+
+            } else if (opcion == 2){
+                addUser();
+            }
+        }
+
+    }
+
+    public static boolean loggearse() throws SQLException {
+        String usuario;
+        String password;
+        Scanner sc = new Scanner(System.in);
+        java.sql.Connection con = Main.connection;
+
+        System.out.println("Introduzca el nombre de usuario: ");
+        usuario = sc.nextLine();
+        System.out.println("Introduzca la contraseña de usuario: ");
+        password = sc.nextLine();
+
+        PreparedStatement st = null;
+        String buscarUser =
+                "SELECT nombre, contrasenya FROM usuarios WHERE nombre = ? AND contrasenya = ?";
+        st = con.prepareStatement(buscarUser);
+        st.setString(1, usuario);
+        st.setString(2, password);
+        ResultSet rs = st.executeQuery();
+        return rs.next();
+    }
+
+    public static void addUser() throws SQLException {
+        String usuario;
+        String apellidos;
+        String password;
+        Scanner sc = new Scanner(System.in);
+        java.sql.Connection con = Main.connection;
+
+        System.out.println("Introduzca el nombre de usuario: ");
+        usuario = sc.nextLine();
+        System.out.println("Introduzca el apellidos de usuario: ");
+        apellidos = sc.nextLine();
+        System.out.println("Introduzca la contraseña de usuario: ");
+        password = sc.nextLine();
+
+        PreparedStatement st = null;
+        String anyadirUser =
+                "INSERT INTO usuarios (nombre, apellidos, contrasenya) VALUES (?, ?, ?)";
+        st = con.prepareStatement(anyadirUser);
+        st.setString(1, usuario);
+        st.setString(2, apellidos);
+        st.setString(3, password);
+        st.executeUpdate();
+        st.close();
+    }
+}
